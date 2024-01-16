e5688b6d098e021f1eceeb222a0dcf564a9a565d
Caner Ağkaya <dev.caneragkaya@gmail.com>
2024-01-16 13:32:31 +0300

    Initial Commit

---
+++ MalatyaLife/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  MalatyaLife
+//
+//  Created by Caner Ağkaya on 16.01.2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
