33211fa233044f545d9629c45991d3a909673bab
Caner Ağkaya <dev.caneragkaya@gmail.com>
2024-01-16 18:51:57 +0300

    Included Firebase

--- MalatyaLife/App/MalatyaLifeApp.swift
+++ MalatyaLife/App/MalatyaLifeApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct MalatyaLifeApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
