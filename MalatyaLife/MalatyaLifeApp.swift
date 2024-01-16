36dc148777984e7f4625c275bbf42b517c5e0dfd
Caner Ağkaya <dev.caneragkaya@gmail.com>
2024-01-16 13:18:05 +0300

    Initial Commit

---
+++ MalatyaLife/MalatyaLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MalatyaLifeApp.swift
+//  MalatyaLife
+//
+//  Created by Caner Ağkaya on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MalatyaLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
